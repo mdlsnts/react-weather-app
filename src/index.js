8921379379985164cf4bae382a1f7c4ff95fe982
mdlsnts <chmielowiec.marta@gmail.com>
2023-05-10 14:04:15 +0200

    Removed start up files and added basic functionality of new files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Weather from "./Weather";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Weather Search Engine</h1>
+    <Weather />
+    <footer>
+      Project coded by&nbsp;
+      <a
+        href="https://linktr.ee/martadlsnts"
+        target="_blank"
+        rel="noreferrer"
+        class="footer-link"
+      >
+        Marta Delos Santos
+      </a>
+      , open-sourced on&nbsp;
+      <a
+        href="https://github.com/mdlsnts/shecodes-react-app"
+        target="_blank"
+        rel="noreferrer"
+        class="footer-link"
+      >
+        Github
+      </a>
+    </footer>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
